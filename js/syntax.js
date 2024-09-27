40983b3ebb2f762d4fb3a1ce95a866d5091394ef
cbspace <24204459+cbspace@users.noreply.github.com>
2024-09-27 21:55:59 +1000

    Syntax Highlighting updated

--- js/syntax.js
+++ js/syntax.js
@@ -1,32 +1,105 @@
-// Syntax highlighting for Python code blocks
-function syntax_highlight() {
-    var lf = '\n';
-    var keywords = ['from', 'import', 'return']
-    var declarations = ['class', 'def']
-    var operators = ['=']
+////////////////////////////////////////////////
+// Syntax Highlighting for Python code blocks //
+////////////////////////////////////////////////
 
-    var code_block = document.getElementById('python');
-    var lines = code_block.innerHTML.split(lf);
-    code_block.innerHTML = '';
+const ParserContext = Object.freeze({
+    None: 0,
+    InStringSingleQuote: 1,
+    InStringDoubleQuote: 2
+});
+
+// Syntax highlighting for Python code blocks
+// Need to use buffer as the browswer checks for
+// HTML tag completion with each write
+function syntax_highlight() {
+    let lf = '\n';
+    let context = ParserContext.None;
+    let keywords = ['from', 'import', 'return']
+    let declarations = ['class', 'def']
+    let operators = ['=']
+
+    let code_block = document.getElementById('python');
+    let lines = code_block.innerHTML.split(lf);
+    let buffer = '';
 
     for (line_idx in lines) {
-        words = lines[line_idx].split(' ');
+        let words = lines[line_idx].split(' ');
 
         for (word_idx in words) {
-            word = words[word_idx];
+            let word = words[word_idx];
 
             if (keywords.includes(word)) {
-                code_block.innerHTML += '<keyword>' + word + '</keyword>';
+                if (context == ParserContext.None) {
+                    buffer += '<keyword>' + word + '</keyword>';
+                } else { 
+                    buffer += word; 
+                }
             } else if (declarations.includes(word)) {
-                    code_block.innerHTML += '<declaration>' + word + '</declaration>';
-            } else if (operators.includes(word)) {
-                    code_block.innerHTML += '<operator>' + word + '</operator>';
+                if (context == ParserContext.None) {
+                    buffer += '<declaration>' + word + '</declaration>';
+                } else { 
+                    buffer += word; 
+                }
             } else {
-                code_block.innerHTML += word;
+                for (char_idx in word) {
+                    let char = word[char_idx];
+
+                    // Look for strings 
+                    if (char == '\'') {
+                         switch (context) {
+                            case ParserContext.None:
+                                context = ParserContext.InStringSingleQuote;
+                                buffer += '<string>\'';
+                                break;
+                            case ParserContext.InStringSingleQuote: 
+                                if (!(is_escaped(word, char_idx))) {
+                                    context = ParserContext.None;
+                                    buffer += '\'</string>';
+                                } else {
+                                    buffer += '\'';
+                                }
+                                break;
+                            case ParserContext.InStringDoubleQuote:
+                                buffer += '\'';
+                                break;
+                         }
+                    } else if (char == '\"') {
+                        switch (context) {
+                            case ParserContext.None:
+                                context = ParserContext.InStringDoubleQuote;
+                                buffer += '<string>\"';
+                                break;
+                            case ParserContext.InStringDoubleQuote:
+                               if (!(is_escaped(word, char_idx))) {
+                                    context = ParserContext.None;
+                                    buffer += '\"</string>';
+                                } else {
+                                   buffer += '\"';
+                                }
+                                break;
+                            case ParserContext.InStringSingleQuote:
+                                buffer += '\"';
+                                break;
+                        }
+                    } else {
+                         buffer += char;
+                    }
+                }
             }
 
-            code_block.innerHTML += ' ';
+            buffer += ' ';
         }
-        code_block.innerHTML += lf;
+        buffer += lf;
     }
+    code_block.innerHTML = buffer;
+}
+
+// Check if a character is escaped
+function is_escaped(word, char_idx) {
+    if (char_idx == 0) {
+        return false;
+    } else if (!(word[char_idx-1] == '\\')) {
+        return false;
+    }
+    return true;
 }
