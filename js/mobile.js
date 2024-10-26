0dd85b14c2f2aed10bbba7b88808aa249d881859
cbspace <24204459+cbspace@users.noreply.github.com>
2024-10-26 21:20:52 +1000

    Mobile template updated

--- js/mobile.js
+++ js/mobile.js
@@ -2,69 +2,76 @@
 let sidebar_visible = false;
 
 // Make website more readable on mobile devices
-function check_device() {
-   var is_mobile = false;
+function is_mobile() {
+    var device_is_mobile = false;
 
-  if (navigator.userAgent.match(/Android/i)
-   || navigator.userAgent.match(/webOS/i)
-   || navigator.userAgent.match(/iPhone/i)
-   || navigator.userAgent.match(/iPad/i)) 
-  {
-     is_mobile = true ;
-     update_for_mobile();
-   }
+    if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)) 
+    {
+        device_is_mobile = true ;
+    }
 
-   // For local testing on PC
-   //update_for_mobile();
+    // For local testing on PC
+    //device_is_mobile = true;
+
+    return device_is_mobile;
 }
 
 function update_for_mobile() {
-   // Change element widths
-   let main_element = document.getElementById('maincont');
-   main_element.className = 'maincont mobile';
-   
-   // Increase font sizes
-   document.body.className = 'body mobile';
-   
-   let linkbold_elements = document.getElementsByClassName('linkbold');
-   for (block_idx=0; block_idx < linkbold_elements.length; block_idx++) {
-       linkbold_elements[block_idx].className = 'linkbold mobile';
-   }
-   
-   let code_elements = document.getElementsByTagName('code');
-   for (block_idx=0; block_idx < code_elements.length; block_idx++) {
-       code_elements[block_idx].className = 'mobile';
-   }
-   
-   let codeblock_elements = document.getElementsByTagName('codeblock');
-   for (block_idx=0; block_idx < codeblock_elements.length; block_idx++) {
-       codeblock_elements[block_idx].className = 'mobile';
-   }
-   
-   document.getElementById('footer').className = 'footer mobile';
-   
-   // Update Sidepanel for mobile
-   document.getElementById('sidepanel').className = 'sidepanel mobile_hide';
-   document.getElementById('iframecontainer').className = 'iframecontainer mobile_hide';
+    if (!is_mobile()) { return; }
 
-//    let sidepanel_element = document.getElementByName('sidepanel');
-//    let sp_headings = sidepanel_element.contentWindow.document.getElementsByClassName('paneltopic');
+    // Change element widths
+    document.getElementById('maincont').className = 'maincont mobile_hide';
 
-//    sidepanel_element.contentWindow.document.body.style.fontSize = '16pt';
+    // Increase font sizes
+    document.body.className = 'body mobile';
 
-//    for (block_idx=0; block_idx < sp_headings.length; block_idx++) {
-//        sp_headings[block_idx].style.fontSize = '18pt';
-//    }
+    let linkbold_elements = document.getElementsByClassName('linkbold');
+    for (block_idx=0; block_idx < linkbold_elements.length; block_idx++) {
+        linkbold_elements[block_idx].className = 'linkbold mobile';
+    }
+
+    let code_elements = document.getElementsByTagName('code');
+    for (block_idx=0; block_idx < code_elements.length; block_idx++) {
+        code_elements[block_idx].className = 'mobile';
+    }
+
+    let codeblock_elements = document.getElementsByTagName('codeblock');
+    for (block_idx=0; block_idx < codeblock_elements.length; block_idx++) {
+        codeblock_elements[block_idx].className = 'mobile';
+    }
+
+    // Show the sidepanel button
+    document.getElementById('expandbutton').style.display = 'block';
+    
+    // Update Sidepanel for mobile
+    document.getElementById('sidepanel').className = 'sidepanel mobile_hide';
+    document.getElementById('iframecontainer').className = 'iframecontainer mobile_hide';
+    
+    // Update footer
+    document.getElementById('footer').className = 'footer mobile';
 }
 
 function toggle_sidebar() {
+    if (!is_mobile()) { return; }
+
     if (sidebar_visible) {
         document.getElementById('sidepanel').className = 'sidepanel mobile_hide';
         document.getElementById('iframecontainer').className = 'iframecontainer mobile_hide';
+        document.getElementById('maincont').className = 'maincont mobile_hide';
         sidebar_visible = false;
     } else {
         document.getElementById('sidepanel').className = 'sidepanel mobile_show';
         document.getElementById('iframecontainer').className = 'iframecontainer mobile_show';
+        document.getElementById('maincont').className = 'maincont mobile_show';
         sidebar_visible = true;
     }
+}
+
+// Called from sidepanel.html
+function update_sidepanel_for_mobile() {
+    if (!is_mobile()) { return; }
+    document.body.className = 'panel_mobile';
 }
