5b0955fdd1e32d2436db34addac86661ba39df1b
cbspace <24204459+cbspace@users.noreply.github.com>
2024-10-26 16:36:14 +1000

    Testing for mobile rendering

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,4 @@
-// Temporary hack to make website more readable on mobile devices
+// Make website more readable on mobile devices
 function check_device() {
    var is_mobile = false;
 
@@ -10,40 +10,44 @@ function check_device() {
      is_mobile = true ;
      update_for_mobile();
    }
+
+   // For local testing on PC
+   //update_for_mobile();
+   answer.innerHTML = is_mobile;
 }
 
 function update_for_mobile() {
    // Change element widths
    let main_element = document.getElementById('maincont');
-   main_element.style.width = '70%';
+   main_element.className = 'maincont mobile';
    
    // Increase font sizes
-   document.body.style.fontSize = '22pt';
+   document.body.className = 'body mobile';
    
    let linkbold_elements = document.getElementsByClassName('linkbold');
    for (block_idx=0; block_idx < linkbold_elements.length; block_idx++) {
-       linkbold_elements[block_idx].style.fontSize = '24pt';
+       linkbold_elements[block_idx].className = 'linkbold mobile';
    }
    
    let code_elements = document.getElementsByTagName('code');
    for (block_idx=0; block_idx < code_elements.length; block_idx++) {
-       code_elements[block_idx].style.fontSize = '20pt';
+       code_elements[block_idx].className = 'mobile';
    }
    
    let codeblock_elements = document.getElementsByTagName('codeblock');
    for (block_idx=0; block_idx < codeblock_elements.length; block_idx++) {
-       codeblock_elements[block_idx].style.fontSize = '18pt';
+       codeblock_elements[block_idx].className = 'mobile';
    }
    
-   document.getElementById('footer').style.fontSize = '16pt';
+   document.getElementById('footer').className = 'footer mobile';
    
-   // Update Sidepanel font sizes
-   let sidepanel_element = document.getElementByName('sidepanel');
-   let sp_headings = sidepanel_element.contentWindow.document.getElementsByClassName('paneltopic');
+//    // Update Sidepanel font sizes
+//    let sidepanel_element = document.getElementByName('sidepanel');
+//    let sp_headings = sidepanel_element.contentWindow.document.getElementsByClassName('paneltopic');
 
-   sidepanel_element.contentWindow.document.body.style.fontSize = '16pt';
+//    sidepanel_element.contentWindow.document.body.style.fontSize = '16pt';
 
-   for (block_idx=0; block_idx < sp_headings.length; block_idx++) {
-       sp_headings[block_idx].style.fontSize = '18pt';
-   }
+//    for (block_idx=0; block_idx < sp_headings.length; block_idx++) {
+//        sp_headings[block_idx].style.fontSize = '18pt';
+//    }
 }
