2fa9570b4d84255578c70cbb31436063905c1354
cbspace <24204459+cbspace@users.noreply.github.com>
2024-11-30 18:54:03 +1000

    Tensor Dimensions Page Updated

---
+++ js/expand.js
@@ -0,0 +1,18 @@
+// Show or hide a section within a page, when the arrow symbol is clicked
+function expand(elem)
+{
+    arr = document.getElementById(elem + "_arr");
+    elem_alt = document.getElementById(elem + "_alt");
+    elem = document.getElementById(elem);
+
+    if (elem.style.display == 'none') {
+        elem_alt.style.display = 'none';
+        elem.style.display = 'block';
+        arr.innerHTML = "&#128316;";
+    } else {
+        elem_alt.style.display = 'block';
+        elem.style.display = 'none';
+        arr.innerHTML = "&#128317;";
+    }
+    alert(t);
+}
